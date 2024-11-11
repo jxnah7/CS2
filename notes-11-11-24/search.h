3ce8338e94693ec6d4caf976cda05aaf8de56174
j.xnah7 <jonahsendejo@gmail.com>
2024-11-11 16:07:03 -0600

    up

--- notes-11-11-24/search.h
+++ notes-11-11-24/search.h
@@ -20,3 +20,25 @@ bool seqSearchLoop(int arr[], int length, int start, int target){
   // RECURISVE CASE
     // take one step
     // call function to do the rest
+  
+bool seqSearchR(int arr[], int length, int start, int target){
+  // BASE CASE
+    // check if my start is at the end of array (or beyond)
+  if(start >= length){
+    return false;
+  }
+  else if(start == length-1){
+    // we only have one number to check
+    if(arr[start] == target){
+      return true;
+    }
+  }
+  // RECURSIVE CASE
+  else{
+    // check one number
+    // the rest is---
+    return seqSearchR(arr, length, start+1, target);
+    // alternatively
+    return (arr[start] == target) || seqSearchR(arr, length, start+1, target);
+  }  
+}
---
+++ notes-11-11-24/sortAlgo.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// sort some numbers
+// take one card from the top of the deck
+// put it face up on a second pile
+
+// 2. take the next card from the top of the deck
+  // CASE 1: the card goes after the previous card
+  // CASE 2: the card goes before the previous card
+
+
+// 3. take the next card
+  // start from the top of the sorted deck 
+  // insert it wherever it goes 
+  // (greater than previous but less than current card)
+
