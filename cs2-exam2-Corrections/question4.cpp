5078d8726d6dd4a6a04b8633b021286fa3b49a27
j.xnah7 <jonahsendejo@gmail.com>
2024-11-18 07:21:11 -0600

    up

---
+++ cs2-exam2-Corrections/question4.cpp
@@ -0,0 +1,14 @@
+/*
+QUESTION: What line of code makes the character pointer studentPointer point to the character variable 
+userStudent?
+
+char userStudent = 'S';
+char *studentPointer;
+
+INCORRECT ANSWER: *studentPointer = &userStudent;
+
+CORRECT ANSWER: studentPointer = &userStudent;
+
+EXPLANATION:
+
+*/
