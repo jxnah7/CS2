34b31baf613194d56e619383309c67599b9a31cd
j.xnah7 <jonahsendejo@gmail.com>
2024-11-18 07:36:13 -0600

    update

--- cs2-exam2-Corrections/question4.cpp
+++ cs2-exam2-Corrections/question4.cpp
@@ -11,4 +11,7 @@ CORRECT ANSWER: studentPointer = &userStudent;
 
 EXPLANATION:
 
+  using *studentPointer = &userStudent; actually dereferences the pointer and thats not what we want
+  using *studentPointer = userStudent; also doesnt work as it stores S into the pointer but the pointer has no valid address yet
+  using studentPointer = &userStudent; this makes the pointer point to the memory address of userStudent, that way when you dereference *studentPointer then we get the char we want which is 'S'
 */
