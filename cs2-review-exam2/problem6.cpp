13d082126e7bad9d1ce64e4c8f91aeeed8ab12a3
j.xnah7 <jonahsendejo@gmail.com>
2024-11-17 21:13:26 -0600

    up

--- cs2-review-exam2/problem6.cpp
+++ cs2-review-exam2/problem6.cpp
@@ -2,7 +2,7 @@
 
 /*
 
-Assume you are given a setup for a stack (with a dynamic array). Be prepared to be able
+Assume you are given a setup for a stack (with a dynamic arrayay). Be prepared to be able
 to either explain (in plain language) and/or implement the following:
 a. Push new items into the stack
 b. “pop” or remove items from the stack
@@ -13,8 +13,67 @@ c. Resize the stack if needed
 /*
 ANSWER:
   in plain english:
-a) Pushing a new item onto a stack is adding another item ontop, similar to a stack of pancakes you can only add from top
-b) popping from the stack is taking from the top or most recent item, again like pancakes you can only remove from the top
+a) Pushing a new item onto a stack is adding another item onpeak, similar to a stack of pancakes you can only add from peak
+b) popping from the stack is taking from the peak or most recent item, again like pancakes you can only remove from the peak
 c) if the stack is full, we need to resize the amount of pancakes we can add, so we get a bigger plate, or if we have too little then we get a smaller plate
 
 */
+
+#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+    int* array;
+    int size;
+    int peak;
+
+public:
+    Stack(int size = 10) {
+        array = new int[size];
+        size = size;
+        peak = -1;
+    }
+
+    ~Stack() {
+        delete[] array;
+    }
+
+    void push(int value) {
+        if (peak == size - 1) {
+            resize();
+        }
+        array[++peak] = value;
+        cout << "Pushed: " << value << endl;
+    }
+
+    int pop() {
+        if (peak == -1) {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        }
+        return array[peak--];
+    }
+
+    void resize() {
+        int newsize = size * 2;
+        int* newarray = new int[newsize];
+        for (int i = 0; i <= peak; i++) {
+            newarray[i] = array[i];
+        }
+        delete[] array;
+        array = newarray;
+        size = newsize;
+    }
+};
+
+int main() {
+    Stack stack;
+
+    stack.push(10); // add to top 10
+
+    cout << "Popped: " << stack.pop() << endl; // pop top off
+    stack.push(12);
+
+    return 0;
+}
