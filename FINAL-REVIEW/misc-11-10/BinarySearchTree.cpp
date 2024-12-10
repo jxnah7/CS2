e117032f6afaf6d8d4fee091e54e1d869eab61b9
j.xnah7 <jonahsendejo@gmail.com>
2024-12-10 15:33:55 -0600

    linkedlist-completo

---
+++ FINAL-REVIEW/misc-11-10/BinarySearchTree.cpp
@@ -0,0 +1,5 @@
+class treeNode {
+  public:
+    int data;
+    treeNode *next;
+};
---
+++ FINAL-REVIEW/misc-11-10/doublyLinkedList.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+};
+
+int main(){
+  cout << "Im trying to make a linked list" << endl;
+
+  Node *head = new Node();  
+  Node *current;
+  Node *first = new Node();
+  Node *second = new Node();
+  Node *third = new Node();
+  Node *tail = new Node();
+
+  head->data = 12;
+  head->next = first;
+  head->prev = nullptr;
+
+  first->data = 13;
+  first->next = second;
+  first->prev = head;
+
+  second->data = 14;
+  second->next = third;
+  second->prev = first;
+
+  third->data = 15;
+  third->next = tail;
+  third->prev = second;
+
+  tail->data = 16;
+  tail->next = nullptr;
+  tail->prev = third;
+
+  current = head;
+  while(current != nullptr){
+    cout << current->data << " ";
+    current = current->next;
+  }
+
+  cout << endl;
+
+  current = tail;
+  while(current != nullptr){
+    cout << current->data << " ";
+    current = current->prev;
+  }
+
+
+  delete head;
+  delete first;
+  delete second;
+  delete third;
+  delete tail;
+  delete current;
+  
+  return 0;
+}
